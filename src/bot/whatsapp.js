d3c31f0f8428693e376489ebb37893fa8cea387c
Filipinho <filipeassad@gmail.com>
2026-04-03 10:28:40 -0400

    feat: add configs novas para o bot

--- src/bot/whatsapp.js
+++ src/bot/whatsapp.js
@@ -16,6 +16,7 @@ if (fs.existsSync(configPath)) {
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
+        headless: true,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
